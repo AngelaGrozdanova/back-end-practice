28675e317273323e1406365c006164638438408f
AngelaGrozdanova <angelagrozdanova06@gmail.com>
2025-09-17 12:48:11 +0300

    Uploads

--- 01.GlowAlchemy_Resources/src/constants.js
+++ 01.GlowAlchemy_Resources/src/constants.js
@@ -1,4 +1,4 @@
-exports.PORT = 3001;
+exports.PORT = 3000;
 exports.DB_Connection_String = "mongodb://localhost:27017/glowAlchemy";
 // exports.DB_Connection_String = "mongodb+srv://angelagrozdanova06_db_user:ZD15QUHXmhyA5LGb@coursebook.vrwyezd.mongodb.net/"
 exports.JWT_Secret = "anckdsnvoehfierhosenvdlds";
